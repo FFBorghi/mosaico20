54a8a427397b4e17324821b4fb9e8463711ffe79
FFBorghi <32781501+FFBorghi@users.noreply.github.com>
2020-12-13 11:47:41 -0300

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import styles from '../styles/style.css'
+import stylesR from '../styles/reset.css'
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
